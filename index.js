285847c1bbeb45e10e8bd2af856572f1d6ffda91
alirezahamidi <alirezahamidi@programmer.net>
2023-12-11 10:33:35 +0330

    some fixes on algorithem

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const c2 = color(process.argv[reverse ? 3 : 2]);
 const name = process.argv[4] ?? 'css-color-gen';
 const steps = 10;
 const rgbDiff = [c1.red() - c2.red(), c1.green() - c2.green(), c1.blue() - c2.blue()];
-const rgbGap = rgbDiff.map(x => Math.round(x / steps));
+const rgbGap = rgbDiff.map(x => Math.floor(x / (steps-1)));
 
 let s = ':host {\r\n';
 s += `--${name}-${(steps - 1) * 100} : ${c2.hex()};\r\n`;
 for (let i = steps - 2; i >= 0; i -= 1) {
+    // console.log(`${c1.red() - (rgbGap[0] * i)},${c1.green() - (rgbGap[1] * i)},${c1.blue() - (rgbGap[2] * i)})`);
     s += `--${name}-${i * 100 + (i == 0 ? 50 : 0)} : ${color(`rgb(${c1.red() - (rgbGap[0] * i)},${c1.green() - (rgbGap[1] * i)},${c1.blue() - (rgbGap[2] * i)})`).hex()};\r\n`;
 }
 s += '}';
